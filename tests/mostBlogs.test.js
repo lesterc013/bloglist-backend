65cd3396fe0dabb1a09ddda18972a41fcb586062
lesterc013 <lesterc013@gmail.com>
2024-06-26 19:16:31 +0800

    Test for most blog count

---
+++ tests/mostBlogs.test.js
@@ -0,0 +1,153 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../util/list_helper')
+
+const oneBlog = [{
+  _id: '5a422a851b54a676234d17f7',
+  title: 'React patterns',
+  author: 'Michael Chan',
+  url: 'https://reactpatterns.com/',
+  likes: 7,
+  __v: 0
+},]
+
+const manyBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+]
+
+const sameAmtBlogsTwoAuthors = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  },
+  {
+    _id: 'for testing',
+    title: 'Djisktra extra',
+    author: 'Edsger W. Dijkstra',
+    url: 'teting',
+    likes: 99,
+    __v: 0
+  },
+]
+
+/**
+ * Most Likes Tests
+ */
+describe('most blogs', () => {
+  test('of empty list is zero', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs([]), 0)
+  })
+
+  test('when list only has one blog, should return that same author and count', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs(oneBlog), {
+      author: 'Michael Chan',
+      blogs: 1
+    })
+  })
+
+  test('when list has many blogs', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs(manyBlogs), {
+      author: 'Robert C. Martin',
+      blogs: 3
+    })
+  })
+
+  // Edsger is seen earlier than Robert in the map -- blog count of Robert is 3 which is not > value therefore Edsger doesnt get replaced
+  test('when list has two authors with same number of blogs', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs(sameAmtBlogsTwoAuthors), {
+      author: 'Edsger W. Dijkstra',
+      blogs: 3
+    })
+  })
+})
--- util/list_helper.js
+++ util/list_helper.js
@@ -1,6 +1,6 @@
-// const dummy = (blogs) => {
-//   return 1
-// }
+const dummy = () => {
+  return 1
+}
 
 const totalLikes = (blogs) => {
   // Given an array of blog posts, Return the total sum of likes in each blog
@@ -25,7 +25,49 @@ const mostLikes = (blogs) => {
   return blogs.find(blog => blog.likes === highestLikes)
 }
 
+/**
+ * Find the author who has the largest amount of blogs
+ * Use a map? For loop to iterate over each blog
+ * if map.has(blog.author)
+ *  +1 to the map.set(blog.author, map.get(blog.author)+1)
+ * else i.e. the map dont have the author
+ *  map.set(blog.author, 0)
+ */
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return 0
+
+  let blogCount = 0
+  let author = ''
+  let authorAndBlogCounts = new Map()
+
+  const compareBlogCounts = (value, key) => {
+    if (value > blogCount) {
+      blogCount = value
+      author = key
+    }
+  }
+
+  for (let i = 0; i < blogs.length; i++) {
+    const thisAuthor = blogs[i].author
+    if (authorAndBlogCounts.has(thisAuthor)) {
+      authorAndBlogCounts.set(thisAuthor, authorAndBlogCounts.get(thisAuthor)+1)
+    }
+    else {
+      authorAndBlogCounts.set(thisAuthor, 1)
+    }
+  }
+
+  authorAndBlogCounts.forEach(compareBlogCounts)
+
+  return {
+    author: author,
+    blogs: blogCount
+  }
+}
+
 module.exports = {
+  dummy,
   totalLikes,
-  mostLikes
+  mostLikes,
+  mostBlogs
 }

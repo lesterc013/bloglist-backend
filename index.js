146f9c5dab21660c24d921f1b5bfd37372ec4900
lesterc013 <lesterc013@gmail.com>
2024-06-26 15:30:11 +0800

    All restructuring completed

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./util/logger')
+const app = require('./app')
 const config = require('./util/config')
-const Blog = require('./models/blogModel')
-const router = require('./controllers/router')
-
-mongoose.connect(config.MONGODB_URI)
-    .then(() => logger.info('Connected to', config.MONGODB_URI))
-    .catch(error => logger.error('Error: ', error.message))
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', router)
+const logger = require('./util/logger')
 
 const PORT = config.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })

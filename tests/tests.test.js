3090f2b50c1e43b7927e4c53626f669f71202961
lesterc013 <lesterc013@gmail.com>
2024-06-26 18:01:19 +0800

    Testing for mostLikes

---
+++ tests/tests.test.js
@@ -0,0 +1,161 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../util/list_helper')
+
+const oneBlog = [{
+  id: '5a422aa71b54a676234d17f8',
+  title: 'Go To Statement Considered Harmful',
+  author: 'Edsger W. Dijkstra',
+  url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+  likes: 5,
+  __v: 0
+}]
+
+const manyBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+]
+
+const manyBlogsButTwoSameLikes = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
+  }
+]
+
+// test('dummy returns one', () => {
+//   const blogs = []
+//   const result = listHelper.dummy(blogs)
+
+//   assert.strictEqual(result, 1)
+// })
+
+/**
+ * Total Likes Tests
+ */
+describe('total likes', () => {
+  // All the tests
+  test('of empty list is zero', () => {
+    const empty = []
+    assert.strictEqual(listHelper.totalLikes(empty), 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    assert.strictEqual(listHelper.totalLikes(oneBlog), 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    assert.strictEqual(listHelper.totalLikes(manyBlogs), 36)
+  })
+})
+
+/**
+ * Most Likes Tests
+ */
+describe('most likes', () => {
+  test('of empty list is zero', () => {
+    assert.deepStrictEqual(listHelper.mostLikes([]), 0)
+  })
+
+  test('when list only has one blog, should return that same blog', () => {
+    assert.deepStrictEqual(listHelper.mostLikes(oneBlog), oneBlog[0])
+  })
+
+  test('when list has many blogs', () => {
+    assert.deepStrictEqual(listHelper.mostLikes(manyBlogs), manyBlogs[2])
+  })
+
+  test('when list has many blogs but two have the same likes', () => {
+    assert.deepStrictEqual(listHelper.mostLikes(manyBlogsButTwoSameLikes), manyBlogsButTwoSameLikes[0])
+  })
+})
--- util/list_helper.js
+++ util/list_helper.js
@@ -1,6 +1,6 @@
-const dummy = (blogs) => {
-  return 1
-}
+// const dummy = (blogs) => {
+//   return 1
+// }
 
 const totalLikes = (blogs) => {
   // Given an array of blog posts, Return the total sum of likes in each blog
@@ -12,7 +12,20 @@ const totalLikes = (blogs) => {
   return likes.reduce((sum, likesOnePost) => sum + likesOnePost)
 }
 
+/**
+ * Given an array of blogs, find out which blog has most likes
+ * Extract out their likes into an array
+ * Use Math.max to find out the max like
+ * Use find to extract out the blog with blog.likes === maxLikes
+ */
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+  const likes = blogs.map(blog => blog.likes)
+  const highestLikes = Math.max(...likes)
+  return blogs.find(blog => blog.likes === highestLikes)
+}
+
 module.exports = {
-  dummy,
-  totalLikes
+  totalLikes,
+  mostLikes
 }

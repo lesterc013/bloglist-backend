0559d39151e951b50932f8bbd36fe85d85102390
lesterc013 <lesterc013@gmail.com>
2024-06-26 14:47:48 +0800

    Config.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./util/logger')
+const config = require('./util/config')
 const Blog = require('./models/blog')
 
-const mongoUrl = 'mongodb+srv://lesterc013:217211@cluster0.cyovdst.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(mongoUrl)
-    .then(() => logger.info('Connected to', mongoUrl))
+// const mongoUrl = 'mongodb+srv://lesterc013:217211@cluster0.cyovdst.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(config.MONGODB_URI)
+    .then(() => logger.info('Connected to', config.MONGODB_URI))
     .catch(error => logger.error('Error: ', error.message))
 
 app.use(cors())
@@ -24,7 +25,7 @@ app.post('/api/blogs', async (request, response) => {
   response.status(201).json(posted)
 })
 
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

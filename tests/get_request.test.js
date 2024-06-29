4caf70c623c756a97b3a2892adb602bf8417d64a
lester <lesterc013@gmail.com>
2024-06-29 20:42:00 +0800

    4.10 Verify POST request is successful

--- tests/get_request.test.js
+++ tests/get_request.test.js
@@ -31,8 +31,6 @@ beforeEach(async () => {
  * Should return the blog posts in JSON format
  */
 test('Make HTTP GET request to /api/blogs', async () => {
-  console.log('Starting test')
-
   // Check if return status and return type is correct i.e. 200 and JSON
   const httpResponse = await api
     .get('/api/blogs')
@@ -44,6 +42,56 @@ test('Make HTTP GET request to /api/blogs', async () => {
 
   // Check for correct amount of blogs returned
   assert.strictEqual(httpResponse.body.length, initialBlogs.length)
+})
+
+/**
+ * Verify if the unique identifier property is named id
+ * Get the response back first -- can just be any GET
+ * Use Object.keys() to get the keys returned in an array
+ * Use assert(array.includes('id))
+ */
+test('Verify unique identifier property is id', async () => {
+  const httpResponse = await api.get('/api/blogs')
+  const firstBlog = httpResponse.body[0] // Just take the first blog
+  const keys = Object.keys(firstBlog)
+  assert(keys.includes('id'))
+})
+
+/**
+ * Verify that HTTP POST request successfully creates new blog post
+ * initialBlogs
+ * Create the note to be posted
+ * POST expect 201, expect Content-Type application/json
+ * Check the whether the returned length is +1 of initial
+ */
+test('Check POST request successful', async () => {
+  const initialBlogs = await helper.getBlogsInJSON()
+
+  // Note: Not posting a Mongoose document but rather a JS object
+  const blog = {
+    title: 'Testing',
+    author: 'Tester',
+    url: 'testing.com',
+    likes: 1
+  }
+  // Check for the correct status and type
+  await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const getResponse = await api.get('/api/blogs')
+
+  // Check if after posting, the get response length is +1 initial length
+  assert.strictEqual(getResponse.body.length, initialBlogs.length+1)
+
+  // Check if the content posted was correct
+  // From the GET response, access the body which are all the blog objects
+  // map the title of the blog objects to an array
+  // Check if 'Testing' is included
+  const titles = getResponse.body.map(blog => blog.title)
+  assert(titles.includes(blog.title))
 })
 
 after(async () => {

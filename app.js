5a46585fc8a9872bd1921cd7176aa58fce347c2b
lester <lesterc013@gmail.com>
2024-07-11 23:12:49 +0800

    Updated for e2e tests

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const middleware = require('./util/middleware')
 const blogRouter = require('./controllers/blogRouter')
 const userRouter = require('./controllers/userRouter')
 const loginRouter = require('./controllers/loginRouter')
+const testingRouter = require('./controllers/testingRouter')
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => logger.info('Connected to', config.MONGODB_URI))
-  .catch(error => logger.error('Error: ', error.message))
+  .catch((error) => logger.error('Error: ', error.message))
 
 app.use(cors())
 app.use(express.json())
@@ -25,6 +27,10 @@ app.use(middleware.extractToken)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
